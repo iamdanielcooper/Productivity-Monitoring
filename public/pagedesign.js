6d42af4f52560b3503cce9ad5550072341565e88
Daniel Cooper <daniel@iMac.local>
2021-01-20 16:36:20 +0000

    style changes

--- public/pagedesign.js
+++ public/pagedesign.js
@@ -1,11 +1,21 @@
-console.log('Hello')
-
-
-function test() {
-    document.getElementById('dropdownBackground').style.bottom = '50px'
-    console.log('test')
+// function to take this as the argument from the HTML, it then processes it and updates everything accordingly.
+function processIncrementor(elm) {
+    // locate the first H2 tag and get the value of the number in it.
+    let location = elm.parentNode.querySelectorAll("h2")[0]
+    let number = parseInt(location.innerText) 
+    
+    // find the class of the obect that was clicked.
+    // if it was the right trigger add. else subtract
+    if (elm.className == 'clickRightforeground') {
+        location.innerText = number + 1
+    } else {
+        if (number <= 0) { // guiard clause catched negative numbers
+            console.log('Err: negative number')
+            return
+        }
+        location.innerText = number - 1
+    }
 }
-
 
 function toggleInfoDropdown(elm) {
     docRef = document.getElementById(elm)
@@ -14,6 +24,14 @@ function toggleInfoDropdown(elm) {
     } else {
         document.getElementById(elm).style.bottom = '300px'
     }
+    rotateArrow()
+}
 
-    console.log('tired')
+function rotateArrow() {
+    if (document.getElementById('arrowImage').style.transform == "rotate(180deg)") {
+        document.getElementById('arrowImage').style.transform = "rotate(360deg)"
+    } else {
+        document.getElementById('arrowImage').style.transform = "rotate(180deg)"
+    }
+    
 }
